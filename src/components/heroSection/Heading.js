fac98cbeecada23cea8b41be51abac8526aa3ccf
Nechir dev <nechirvan.abdullah@outlook.com>
2020-07-15 05:06:11 -0800

    first commit

---
+++ src/components/heroSection/Heading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Heading() {
+    return (
+        <section className="heroHeader">
+            <h1>
+                Designer<br /> 
+                & <br />
+                Front-End Developer
+            </h1>
+        </section>
+    )
+}
+
+export default Heading;
