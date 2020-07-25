b51ed0b575c00d3dd952b00554b071eff71289da
Nechir dev <nechirvan.abdullah@outlook.com>
2020-07-25 07:30:11 -0800

    second commit of July 25

--- src/App.js
+++ src/App.js
@@ -5,37 +5,62 @@ import About from './components/About';
 // routing
 import { BrowserRouter, Link, Route } from 'react-router-dom'
 
+// images
+import MenuImg from './menu.svg'
+import crossImage from './cross.svg'
+import logo from './logo anem.svg'
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <header>
-        {/* logo */}
-        <div className="logo">
-          <a href="/">
-            dev. <strong>NECHIR</strong>
-          </a>
-        </div>
-        {/* menu */}
-        <nav>
-          <div>
-            <img src="" alt="menu" />
+
+export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      isActive: false
+    };
+    this.handleClic = this.handleClic.bind(this);
+  }
+  handleClic(){
+    this.setState(prevState => {
+      return {isActive: !prevState.isActive}
+    });
+
+  }
+  render() {
+
+    return (
+      <BrowserRouter>
+        <header>
+          {/* logo */}
+          <div className="logo">
+            <a href="/">
+              <img src={logo} alt="dev Nechir" />
+            </a>
           </div>
-          <ul className="active">
-            <li><Link to="/">Portfolio</Link></li>
-            <li><Link to="/About">About</Link></li>
-          </ul>
-        </nav>
+          {/* menu */}
+          <nav onClick={this.handleClic}>
+            {/* ------------------------------ */}
+            <div>
+              <img src={MenuImg} alt="menu" />
+            </div>
+            {/* ------------------------------ */}
+            <ul className={this.state.isActive ? "active" : "" }>
+              <li style={{marginBottom: "1rem", textAlign:"right", color: "white"}}>
+                <img src={crossImage} alt="menu" />
+                </li>
+              <li><Link to="/">Portfolio</Link></li>
+              <li><Link to="/About">About</Link></li>
+            </ul>
+          </nav>
+        </header>
 
-      </header>
+        <div>
+          <Route exact path="/" component={Portfolio} />
+          <Route path="/About" component={About} />
+        </div>
+      </BrowserRouter>
 
-      <div>
-        <Route exact path="/" component={Portfolio} />
-        <Route path="/About" component={About} />
-      </div>
-    </BrowserRouter>
-
-  )
+    )
+  }
 }
 
 
