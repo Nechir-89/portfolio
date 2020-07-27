89414722513c86a1a780a723dcecae37e6a49997
Nechir dev <nechirvan.abdullah@outlook.com>
2020-07-27 00:55:42 -0800

    first commit of July 27

--- src/components/classTimeProject/Body.js
+++ src/components/classTimeProject/Body.js
@@ -14,19 +14,32 @@ export default function Body() {
             <h3>Some Screenshots</h3>
             <Card
                 imageURL={image1}
-                desc=""
+                desc="The above page is Home page (index page), in the home page there are
+                two avialable option. first option (Class schedule) to display schedule on
+                TV screens and second option (Update Schedule) is to change schedule."
             />
             <Card
                 imageURL={image2}
-                desc=""
+                desc="
+                        Schedule display page is the first option and it is designed to display
+                        four tables at a time from 8 tables (first four tables will appear first
+                        and after 20 seconds next four tables will appear and current tables will
+                        disappear). time and date also have been set on the page.
+                "
             />
             <Card
                 imageURL={image3}
-                desc=""
+                desc="
+                        Here is the second avialable option from home page. in this page 
+                        user can change schedule data. It is designed to be easy to use
+                        and fast.    
+                "
             />
             <Card
                 imageURL={image4}
-                desc=""
+                desc="On top, right hand corner there is notification, whenever user change 
+                something and click on save button there will be notification for him which
+                shows the state of change (pass or fail) "
             />
         </section>
     )
--- src/components/classTimeProject/Header.js
+++ src/components/classTimeProject/Header.js
@@ -9,11 +9,13 @@ export default function Header() {
                     <h3>Class Time Schedule Project</h3>
                     <p>
                         This project is basically build for computer science department (CS) 
-                        to show class time on TV screens. I designed and developed the whole 
-                        project. For development I used HTML, CSS (SCSS), JQuery, Ajax, JSON, 
-                        PHP, and MySQL database.<br/>
-                        I worked in this project to be local (no host), 
-                        I just used localy on my computer and T.V screen.
+                        to show class time on TV screens. <br />
+                        The project is built in May 2019. I used pen and papre to build wireframe. 
+                        and for development I used 
+                        HTML, CSS (SCSS), JQuery, Ajax, JSON as a front-end almost and WAMP server
+                        to deal with PHP, PHP myadmin, Apache, and MySQL database as the back-end<br/>
+                        I worked and setup everything of the project on my machine which is local,
+                        because I just want to be local and it did not need to be avialable on the internet.
                     </p>
                 </figcaption>
             </figure>
